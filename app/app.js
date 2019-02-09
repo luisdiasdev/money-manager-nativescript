1ba1fa35c846d709495c263a4df94797e589a25e
Luis Gustavo de Oliveira Dias <luis.gustavo.dias@hotmail.com>
2019-02-09 16:59:52 -0200

    Configure lint and code formatter

--- app/app.js
+++ app/app.js
@@ -1,15 +1,14 @@
-import Vue from "nativescript-vue";
+import Vue from 'nativescript-vue';
 
-import Home from "./components/Home";
+import Home from './components/Home';
 
 new Vue({
+  components: {
+    Home,
+  },
 
-    template: `
+  template: `
         <Frame>
             <Home />
         </Frame>`,
-
-    components: {
-        Home
-    }
 }).$start();
